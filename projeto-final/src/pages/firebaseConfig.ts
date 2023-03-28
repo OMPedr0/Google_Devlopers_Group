76764b028ddf54c4cf5ac481e11948e0e0b548af
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2023-03-28 16:12:27 +0100

    Update firebaseConfig.ts

--- projeto-final/src/pages/firebaseConfig.ts
+++ projeto-final/src/pages/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const app = initializeApp({
-    apiKey: "AIzaSyDx4hfHcxWm4quil0H8PS451GNTVBXKDao",
-    authDomain: "project-workshop-9a580.firebaseapp.com",
-    projectId: "project-workshop-9a580",
-    storageBucket: "project-workshop-9a580.appspot.com",
-    messagingSenderId: "801854471530",
-    appId: "1:801854471530:web:6615765b17e4627591f913"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 export const auth = getAuth(app);
